041a8f83e39c2cf375dbabfce1b463e0d0d01cd0
Gregorio <greg.magalhaes@gmail.com>
2024-03-07 00:46:37 -0300

    Adds class to manage bluetooth transport

--- WalletSdk/src/main/java/com/spruceid/wallet/sdk/BaseCredential.kt
+++ WalletSdk/src/main/java/com/spruceid/wallet/sdk/BaseCredential.kt
@@ -2,6 +2,10 @@ package com.spruceid.wallet.sdk
 
 open class BaseCredential constructor(private val id: String?) {
 
+    fun getId(): String? {
+        return this.id
+    }
+
     override fun toString(): String {
         return "Credential($id)"
     }
