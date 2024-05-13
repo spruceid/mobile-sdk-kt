27e27b83b6b3dd1e15d1aff23e330433ab4472b2
Juliano Cézar Chagas Tavares <julianocezargyn@hotmail.com>
2024-05-13 09:40:40 -0300

    Update WalletSdk/src/main/java/com/spruceid/wallet/sdk/ui/QRCodeAnalyzer.kt
    Co-authored-by: Gregório Granado Magalhães <greg.magalhaes@gmail.com>

--- WalletSdk/src/main/java/com/spruceid/wallet/sdk/ui/QRCodeAnalyzer.kt
+++ WalletSdk/src/main/java/com/spruceid/wallet/sdk/ui/QRCodeAnalyzer.kt
@@ -14,12 +14,14 @@ import java.nio.ByteBuffer
 class QrCodeAnalyzer(
     private val onQrCodeScanned: (String) -> Unit,
 ) : ImageAnalysis.Analyzer {
-    private val supportedImageFormats =
-        listOf(
-            ImageFormat.YUV_420_888,
-            ImageFormat.YUV_422_888,
-            ImageFormat.YUV_444_888,
-        )
+
+  private val supportedImageFormats = mutableListOf(YUV_420_888)
+
+  init {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      supportedImageFormats.addAll(listOf(YUV_422_888, YUV_444_888))
+    }
+  }
 
     override fun analyze(image: ImageProxy) {
         if (image.format in supportedImageFormats) {
