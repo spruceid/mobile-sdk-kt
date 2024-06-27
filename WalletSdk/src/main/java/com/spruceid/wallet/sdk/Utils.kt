2293de1160f5168a7a3644fa5a72386a57be0d67
Gregorio <greg.magalhaes@gmail.com>
2024-06-27 15:21:41 -0300

    Add isOnline helper

--- WalletSdk/src/main/java/com/spruceid/wallet/sdk/Utils.kt
+++ WalletSdk/src/main/java/com/spruceid/wallet/sdk/Utils.kt
@@ -28,3 +28,25 @@ enum class PresentmentState {
     /// App should display a success message and offer to close the page
     SUCCESS,
 }
+
+fun isOnline(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (connectivityManager != null) {
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+    }
+    return false
+}
