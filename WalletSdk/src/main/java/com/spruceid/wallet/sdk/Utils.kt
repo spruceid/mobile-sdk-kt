063e50b5625f1e45d5b662de0313766d228dc38e
Gregorio <greg.magalhaes@gmail.com>
2024-07-25 18:54:04 -0300

    Add class to manage connection state

--- WalletSdk/src/main/java/com/spruceid/wallet/sdk/Utils.kt
+++ WalletSdk/src/main/java/com/spruceid/wallet/sdk/Utils.kt
@@ -1,9 +1,5 @@
 package com.spruceid.wallet.sdk
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-
 
 fun hexToByteArray(value: String): ByteArray {
     val stripped = value.substring(2)
@@ -31,21 +27,4 @@ enum class PresentmentState {
 
     /// App should display a success message and offer to close the page
     SUCCESS,
-}
-
-fun isOnline(context: Context): Boolean {
-    val connectivityManager =
-    context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    val capabilities =
-        connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-    if (capabilities != null) {
-        if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-            return true
-        } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-            return true
-        } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
-            return true
-        }
-    }
-    return false
 }
