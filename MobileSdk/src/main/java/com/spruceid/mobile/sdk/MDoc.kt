8a7a7d9d8953ad2c2c2a183d5ce51de2508dffa4
Juliano Cezar Chagas Tavares <julianocezargyn@hotmail.com>
2024-09-25 17:25:36 -0300

    Bump mobile-sdk-rs version

--- MobileSdk/src/main/java/com/spruceid/mobile/sdk/MDoc.kt
+++ MobileSdk/src/main/java/com/spruceid/mobile/sdk/MDoc.kt
@@ -1,14 +1,14 @@
 package com.spruceid.mobile.sdk
 
 import android.util.Log
-import com.spruceid.mobile.sdk.rs.MDoc as InnerMDoc
+import com.spruceid.mobile.sdk.rs.Mdoc as InnerMDoc
 
 class MDoc(id: String, issuerAuth: ByteArray, val keyAlias: String) : BaseCredential(id) {
      val inner: InnerMDoc
 
     init {
         try {
-            inner = InnerMDoc.fromCbor(issuerAuth)
+            inner = InnerMDoc.fromCborEncodedDocument(issuerAuth, keyAlias)
         } catch (e: Throwable) {
             Log.e("MDoc.init", e.toString())
             throw e
