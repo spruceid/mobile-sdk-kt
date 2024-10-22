cecae761ba411539fdbff4ece71775da83926e2a
Tiago Nascimento <tiago.nascimento@spruceid.com>
2024-10-22 19:18:48 -0300

    Update `mobile-sdk-rs` version
    Signed-off-by: Tiago Nascimento <tiago.nascimento@spruceid.com>

--- MobileSdk/build.gradle.kts
+++ MobileSdk/build.gradle.kts
@@ -116,7 +116,7 @@ android {
 }
 
 dependencies {
-    api("com.spruceid.mobile.sdk.rs:mobilesdkrs:0.2.1")
+    api("com.spruceid.mobile.sdk.rs:mobilesdkrs:0.2.2")
     //noinspection GradleCompatible
     implementation("com.android.support:appcompat-v7:28.0.0")
     /* Begin UI dependencies */
